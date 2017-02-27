56ff02f00c33b960aba0534fec78d96d7b8c8a3c
Jonathan <jmthoenen@hotmail.com>
2017-02-27 13:36:09 +0100

    added youtube player

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//top navigatie
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -5,4 +6,18 @@ function myFunction() {
     } else {
         x.className = "topnav";
     }
+}
+/* muziek tabs */
+function openplayer(evt, songName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(songName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
